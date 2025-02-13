bfb3cb04eb67cc021d404d410eb1260ac2014204
Mario76-soldier <74755776+Mario76-soldier@users.noreply.github.com>
2025-02-14 03:08:07 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int dp[1000001];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n;
+	cin >> n;
+	dp[1] = 0;
+	dp[2] = 1;
+	dp[3] = 1;
+	for (int i = 4; i <= n; i++) {
+		if (i % 2 == 0) {
+			if (i % 3 == 0) {
+				dp[i] = min(dp[i - 1], min(dp[i / 2], dp[i / 3]))+1;
+			}
+			else {
+				dp[i] = min(dp[i - 1], dp[i / 2]) + 1;
+			}
+		}
+		else if (i % 3 == 0) {
+			dp[i] = min(dp[i - 1], dp[i / 3]) + 1;
+		}
+		else {
+			dp[i] = dp[i - 1] + 1;
+		}
+	}
+	cout << dp[n];
+}
