4f4755be34e016b71e71e6b22c056cab22d02186
Mario76-soldier <74755776+Mario76-soldier@users.noreply.github.com>
2025-02-14 01:41:14 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+int check[10] = { 0, }, arr[10] = { 0, }, inp[10];
+
+void dfs(int a, int len) {
+	if (len==m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	for (int i = a; i < n; i++) {
+		arr[len] = inp[i];
+		dfs(i+1, len + 1);
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> inp[i];
+	}
+	sort(inp, inp + n);
+	dfs(0, 0);
+}
