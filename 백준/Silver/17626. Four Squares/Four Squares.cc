396e5f35e54d8b8d6004f76e108c34e1bd48cc69
Mario76-soldier <74755776+Mario76-soldier@users.noreply.github.com>
2025-02-15 00:47:03 +0900

    [Silver III] Title: Four Squares, Time: 8 ms, Memory: 2216 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int dp[50001];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		dp[i] = dp[i - 1] + 1;
+		for (int j = 1; j * j <= i; j++) {
+			dp[i] = min(dp[i], dp[i - j * j] + 1);
+		}
+	}
+	cout << dp[n];
+}
