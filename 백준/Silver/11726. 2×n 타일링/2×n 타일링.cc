b012bdfd31a615bc75ecfd5255d3a2c5a73cc315
Mario76-soldier <74755776+Mario76-soldier@users.noreply.github.com>
2025-02-15 00:23:18 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int dp[1001];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n;
+	cin >> n;
+	dp[1] = 1;
+	dp[2] = 2;
+	for (int i = 3; i <= n; i++) {
+		dp[i] = dp[i - 1]%10007 + dp[i - 2]%10007;
+	}
+	cout << dp[n]%10007;
+}
