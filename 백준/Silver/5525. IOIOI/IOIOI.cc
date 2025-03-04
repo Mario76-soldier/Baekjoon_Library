01a4b2f3afead637848b74df109548ff1c7aa4c6
Mario76-soldier <74755776+Mario76-soldier@users.noreply.github.com>
2025-03-04 23:43:40 +0900

    [Silver I] Title: IOIOI, Time: 8 ms, Memory: 3680 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.cc
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -8,15 +8,23 @@ int main() {
 	cout.tie(NULL);
 	int n, m, count=0;
 	cin >> n >> m;
-	string start = "I";
 	string s;
-	for (int i = 0; i < n; i++) {
-		start += "OI";
-	}
 	cin >> s;
-	for (int i = 0; i < m-(n*2); i++) {
-		if (s.substr(i, 1 + (n * 2)) == start) {
-			count++;
+	for (int i = 0; i < m; i++) {
+		int leng = 0;
+		if (s[i] == 'O') {
+			continue;
+		}
+		else {
+			while (s[i + 1] == 'O' && s[i + 2] == 'I') {
+				leng++;
+				if (leng == n) {
+					leng--;
+					count++;
+				}
+				i += 2;
+			}
+			leng = 0;
 		}
 	}
 	cout << count;
