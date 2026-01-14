d80b0eae5c3d8b57f34861273949e0e46d089401
Mario76-soldier <74755776+Mario76-soldier@users.noreply.github.com>
2026-01-14 21:54:50 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -0,0 +1,1 @@
+select distinct a.cart_id from (SELECT cart_id from cart_products where name="Yogurt") a join (select cart_id from cart_products where name="Milk") b on a.cart_id=b.cart_id order by a.cart_id
