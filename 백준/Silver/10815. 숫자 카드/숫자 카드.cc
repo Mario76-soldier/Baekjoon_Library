0dd20af53b17ac31dc8b3d5654f98e4684b422a0
Mario76-soldier <74755776+Mario76-soldier@users.noreply.github.com>
2026-02-04 11:20:54 +0900

    [Silver V] Title: 숫자 카드, Time: 360 ms, Memory: 25520 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.cc
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -2,27 +2,30 @@
 #include <set>
 using namespace std;
 
-int main() {
-	ios::sync_with_stdio(false);
+int main(){
+    ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
+    
+    int n;
+    cin>>n;
+    set<int> s;
+    int a;
+    for(int i=0; i<n; i++){
+        cin>>a;
+        s.insert(a);
+    }
+    int m;
+    cin>>m;
+    int b;
+    for(int i=0; i<m; i++){
+        cin>>b;
+        if(s.find(b)!=s.end()){
+            cout<<"1 ";
+        }
+        else{
+            cout<<"0 ";
+        }
+    }
 
-	set<int> s;
-	int n, a;
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cin >> a;
-		s.insert(a);
-	}
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cin >> a;
-		if (s.find(a) != s.end()) {
-			cout << "1 ";
-		}
-		else {
-			cout << "0 ";
-		}
-	}
-	return 0;
 }
