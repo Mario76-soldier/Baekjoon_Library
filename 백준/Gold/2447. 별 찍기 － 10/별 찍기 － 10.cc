26a2569a99f0b029cd0a14f759b20bf5c743e42f
Mario76-soldier <74755776+Mario76-soldier@users.noreply.github.com>
2026-02-06 16:50:02 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 184 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
@@ -2,29 +2,26 @@
 #include <cmath>
 using namespace std;
 
-void func(int i, int j, int n) {
-	if ((i / n) % 3 == 1 && (j / n) % 3 == 1) {
-		cout << " ";
-	}
-	else if (n <= 1) {
-		cout << "*";
-	}
-	else {
-		func(i, j, n / 3);
-	}
-}
-
-int main() {
-	ios::sync_with_stdio(false);
+int main(){
+    ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			func(i, j, n);
-		}
-		cout << "\n";
-	}
-	return 0;
+    
+    int n;
+    cin>>n;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            int size=n;
+            while(size>1 && ((i/size)%3!=1 || (j/size)%3!=1)){
+                size/=3;
+            }
+            if((i/size)%3==1 && (j/size)%3==1){
+                cout<<' ';
+            }
+            else{
+                cout<<'*';
+            }
+        }
+        cout<<'\n';
+    }
 }
